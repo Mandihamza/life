718103f7d25211d6f0bda7fece7d669ab29d6fee
mandihamza <mandihamza@gmail.com>
2020-09-26 12:01:00 -0600

    Completes MVPs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { DrawGrid } from './components/game/DrawGrid'
 function App(props) {
   return (
   <>
-    <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', maxWidth: '80vw'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', maxWidth: '80vw', marginLeft: '10vw'}}>
     <DrawGrid />
     </div>
   </>
