a3fe748713e8d882afc97e33da87ff70c9888b01
mandihamza <mandihamza@gmail.com>
2020-09-28 17:40:23 -0600

    Updates

---
+++ precache-manifest.755532dbdd7db19b147767626d97e952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea871c99e6ab63487a64a5f3ac08846",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "11576dd98d5394195ad6",
+    "url": "/life/static/js/2.11b2c8f0.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/life/static/js/2.11b2c8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821f3a126a325469d731",
+    "url": "/life/static/js/main.3ae86f93.chunk.js"
+  },
+  {
+    "revision": "d6e8893058b715051ad3",
+    "url": "/life/static/js/runtime-main.dae7108a.js"
+  }
+]);
